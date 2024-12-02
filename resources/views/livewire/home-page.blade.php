65e1b2ac91f76f47b0e3ff50df13bab01f42a8a9
Samuel Robayo <samirobayo04@gmail.com>
2024-12-02 10:37:58 +0100

    small mod on homepage

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -9,18 +9,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 @if ($posts->isEmpty())
-                    <div class="text-center">
-                        <p class="text-2xl">You have no posts yet</p>
+                <div class="flex items-center justify-center min-h-screen text-center">
+                    <div>
+                        <h1
+                            class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
+                            There are no posts yet</h1>
+                        <p
+                            class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                            Share your thoughts and join the conversation! 🚀 Your next post could inspire others.
+                        </p>
+                        <form action="{{ route('post.create') }}" method="GET">
+
+                    </div>
+                </div>
                 @endif
                 <div class="p-6 text-gray-900">
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                         @foreach ($posts as $post)
                             <div class="mb-6">
-                                <!-- Envolver toda la carta con un enlace -->
                                 <a href="{{ route('post.visualize', $post->id) }}" class="block">
-                                    <!-- Carta individual con imagen de fondo -->
                                     <div class="max-w-lg mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-                                        <!-- Imagen de fondo en la carta -->
                                         <div class="h-48 bg-cover rounded-t-lg"
                                             style="background-image: url('{{ asset('storage/' . $post->image) }}')"
                                             title="Post image"></div>
